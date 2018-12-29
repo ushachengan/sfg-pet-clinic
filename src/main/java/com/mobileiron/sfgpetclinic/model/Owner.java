53a566555757faede130ab30c95d2db29f410530
Usha Chengan <uchengan@mobileiron.com>
2018-12-28 20:01:08 -0800

    adding POJOs. Closes #2

---
+++ src/main/java/com/mobileiron/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.mobileiron.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/mobileiron/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.mobileiron.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
