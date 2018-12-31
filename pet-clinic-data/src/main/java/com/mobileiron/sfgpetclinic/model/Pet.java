e38c54d39b30689c557a9412b81ee61f811d1340
Usha Chengan <uchengan@mobileiron.com>
2018-12-31 15:27:44 -0800

    Added baseEntity. closes #10

--- pet-clinic-data/src/main/java/com/mobileiron/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/mobileiron/sfgpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.mobileiron.sfgpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
 
     private PetType petType;
     private Owner owner;
